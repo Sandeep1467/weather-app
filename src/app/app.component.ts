9cc288a45a3b42072a0bf81f23c023516a76fa85
Sandeep <p.sandeep1810@gmail.com>
2021-10-28 12:20:39 +0530

    weather and Map Location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'weather-app';
+  @ViewChild('mapContainer', {static: false}) gmap!: ElementRef;
+  map!: google.maps.Map;
+  lat = 40.730610;
+ lng = -73.935242;
+
+ coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+ mapOptions: google.maps.MapOptions = {
+   center: this.coordinates,
+   zoom: 8
+ };
+
+ marker = new google.maps.Marker({
+  position: this.coordinates,
+  map: this.map,
+});
+
+  ngAfterViewInit(){
+    this.mapInitializer();
+  }
+  mapInitializer(){
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
+  }
+  setMapCoordinates(place:any){
+    console.log(place);
+    this.coordinates = new google.maps.LatLng(place.lat, place.lng);
+    this.marker.setPosition(this.coordinates);
+    this.map.setCenter(this.coordinates);
+  }
 }
+ 
+ 
+
+ 
--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { WeatherClientService } from '../weather-client.service';
 import { Subscriber } from 'rxjs';
 
@@ -8,10 +8,15 @@ import { Subscriber } from 'rxjs';
   styleUrls: ['./weather-home.component.css']
 })
 export class WeatherHomeComponent implements OnInit {
-  
+
+  @Output() newCoord = new EventEmitter<any>();
+  newCoordinates(value: any){
+    this.newCoord.emit(value);
+  }
+
   public city: string= "";
   weather: any;
-  constructor( private weatherClient: WeatherClientService) {
+  constructor(private weatherClient: WeatherClientService) {
   }
 
   ngOnInit(): void {
@@ -22,6 +27,7 @@ export class WeatherHomeComponent implements OnInit {
     this.weatherClient.getData(this.city).subscribe(response => {
       this.weather = response;
       console.log(this.weather);
+      this.newCoordinates({ lat: this.weather.coord.lat, lng: this.weather.coord.lon});
     })
   }
 }
