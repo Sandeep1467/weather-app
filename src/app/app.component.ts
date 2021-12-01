5f6c2de0750fb488f22d83b2ebbc13f7f38f894c
Sandeep <p.sandeep1810@gmail.com>
2021-12-01 12:53:22 +0530

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animation } from '@angular/animations';
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 
@@ -8,18 +9,17 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent implements AfterViewInit{
 
+ source: any;
+ destination: any;
+
   title = 'weather-app';
   @ViewChild('mapContainer', {static: false}) gmap!: ElementRef;
+  @ViewChild('myBtn', {static: false}) btnElement!: ElementRef;
+
   map!: google.maps.Map;
   lat = 40.730610;
  lng = -73.935242;
   formattedAddress: string;
-//  startPoint = new google.maps.Marker({
-//    map: this.map
-//  });
-//  endPoint = new google.maps.Marker({
-//    map: this.map
-//  });
 
  coordinates = new google.maps.LatLng(this.lat, this.lng);
 
@@ -29,14 +29,32 @@ export class AppComponent implements AfterViewInit{
  };
 
   marker = new google.maps.Marker({
-  animation: google.maps.Animation.DROP,
-  position: this.coordinates,
-  map: this.map,
-  draggable:true,
-});
-
+    animation: google.maps.Animation.DROP,
+    position: this.coordinates,
+    map: this.map,
+    draggable:true,
+    // icon: '../assets/markerIcon.png'
+    icon: 'http://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+  });
+  
   ngAfterViewInit(){
     this.mapInitializer();
+    this.marker;
+    this.marker.setAnimation(google.maps.Animation.DROP);
+
+    this.btnElement.nativeElement.addEventListener('click',()=>{
+      this.myFunction()
+    })
+
+    // let btn =  document.getElementById("btnElement");
+
+    // btn.addEventListener("click", () =>{
+    //   this.myFunction();
+    // });
+  }
+
+  myFunction(){
+    document.getElementById("demo").innerHTML = "It's Working!";
   }
   mapInitializer(){
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
@@ -69,7 +87,56 @@ export class AppComponent implements AfterViewInit{
       }
     );
   }
+  getSourceMarker($event){
+    console.log($event)
+    this.source=$event;
+  }
+  getDestinationMarker($event){
+    console.log($event)
+    this.destination=$event;
+  }
+  showDirection(){
+    console.log("show direction working")
   
+    let origin = this.source;
+    console.log(origin);
+    let destination = this.destination;
+    console.log(destination);
+    let directionsService: google.maps.DirectionsService = new google.maps.DirectionsService();
+    let request: google.maps.DirectionsRequest = {
+      origin: new google.maps.LatLng(this.source.lat, this.source.lng),
+      destination: new google.maps.LatLng(this.destination.lat, this.destination.lng),
+      travelMode: google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true,
+    };
+    directionsService.route(request, (response, status) => {
+      if (status.toString() == "OK") {
+        console.log(response)
+        let directionRenderer = new google.maps.DirectionsRenderer();
+        directionRenderer.setMap(this.map);
+        directionRenderer.setDirections(response);
+        // this.jobMapService.renderRoute(response);
+        // this.form.get('googleMapsDirectionsResult').setValue(JSON.stringify(response));
+        let legs = response.routes[0].legs;
+        /* this.distance = (legs.reduce((sum, current) => sum + current.distance.value, 0));
+         let distanceKm = (this.distance * 0.001);
+         this.distanceText = distanceKm + " Km";
+         this.estimatedDuration = (legs.reduce((sum, current) => sum + current.duration.value, 0));
+         let num = this.estimatedDuration / 60;
+         let hours = (num / 60);
+         let rhours = Math.floor(hours);
+         let minutes = (hours - rhours) * 60;
+         let rminutes = Math.round(minutes);
+         this.estimatedDurationText = rhours + " h " + rminutes + " m";
+         this.form.get('distance').setValue(this.distance);
+         this.form.get('estimatedDuration').setValue(this.estimatedDuration);*/
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+    );
+  }
+
 }
  
  
--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -8,36 +8,49 @@ import { WeatherClientService } from '../weather-client.service';
 })
 export class WeatherHomeComponent implements OnInit {
 
-  @ViewChild('addresstext', {static: false}) addresstext!:  ElementRef;
-  @Output() newCoord = new EventEmitter<any>();
+  @ViewChild('source', {static: false}) source!:  ElementRef;
+  @ViewChild('destination', {static: false}) destination!:  ElementRef;
 
-  newCoordinates(value: any){
-    this.newCoord.emit(value);
-  }
+ 
+  @Output() sourceMarker = new EventEmitter<any>();
+  @Output() destinationMarker = new EventEmitter<any>();
+  @Output() searchClick = new EventEmitter();
+
+  // newCoordinates(value: any){
+  //   this.newCoord.emit(value);
+  // }
 
   @Input() city: string="";
-  weather: any;
+  weather: any; 
   constructor(private weatherClient: WeatherClientService,private ngZOne:NgZone) {
   }
+  @Input() city2: string=""
 
   ngOnInit(): void {
     console.log("ngOnInit from WeatherComponent");
   }
   ngAfterViewInit():  void {
     this.googlePlaceAutoCompleteAttach();
+    this.googlePlaceAutoCompleteAttach1();
   }
   
   getWeather(){
     this.weatherClient.getData(this.city).subscribe(response => {
       this.weather = response;
       console.log(this.weather);
-      this.newCoordinates({ lat: this.weather.coord.lat, lng: this.weather.coord.lon});
+      this.searchClick.emit();
+      // this.newCoordinates({ lat: this.weather.coord.lat, lng: this.weather.coord.lon});
     })
+  }
+  pressButton(){
+    document.getElementById("City")
+    console.log("Press button is working")
   }
 
   private googlePlaceAutoCompleteAttach():void {
     let autocomplete =
-    new google.maps.places.Autocomplete(this.addresstext.nativeElement, { types: ["(cities)"] });
+    new google.maps.places.Autocomplete(this.source.nativeElement, { types: ["(cities)"] });
+    
     autocomplete.addListener("place_changed",() => {
          
       this.ngZOne.run(() =>{
@@ -48,17 +61,31 @@ export class WeatherHomeComponent implements OnInit {
           return;
         } else {
           console.log(place);
-          this.city = place.formatted_address;
-          // this.address = place.formatted_address;
-          // this.lat = place.geometry.location.lat();
-          // this.lng = place.geometry.location.lng();
-          // this.marker.setPosition(new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng()));
-          // this.map.setCenter(new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng()));
+          this.sourceMarker.emit({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng()});
+          // this.city = place.formatted_address;
         }
-
       });
-       
+    });
+  }
+  private googlePlaceAutoCompleteAttach1():void {
+    let autocomplete =
+    new google.maps.places.Autocomplete(this.destination.nativeElement, { types: ["(cities)"] });
+    
+    autocomplete.addListener("place_changed",() => {
+         
+      this.ngZOne.run(() =>{
 
+        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+        if (place.geometry === undefined || place.geometry === null) {
+          return;
+        } else {
+          console.log(place);
+          this.destinationMarker.emit({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng()});
+
+          // this.city = place.formatted_address;
+        }
+      });
     });
   }
 }
